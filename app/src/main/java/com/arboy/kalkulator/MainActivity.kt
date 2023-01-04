6f98c64f89dc5d7d0a30a4ef3bdd03b4ada75669
Mardiansyah I E. Gunting <mardiansyahmsy@gmail.com>
2023-01-04 17:27:38 +0700

    Progress on MainActivity

--- app/src/main/java/com/arboy/kalkulator/MainActivity.kt
+++ app/src/main/java/com/arboy/kalkulator/MainActivity.kt
@@ -3,11 +3,22 @@ package com.arboy.kalkulator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.arboy.kalkulator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var lastNumeric = false
+    var stateError = false
+    var lastDot = false
+
+    private lateinit var expression: Expression
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     fun onAllclearClick(view: View) {}
