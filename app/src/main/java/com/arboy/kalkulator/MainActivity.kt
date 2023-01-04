c5d33c3603ce71a3826e2e3478ed229739dc71f9
Mardiansyah I E. Gunting <mardiansyahmsy@gmail.com>
2023-01-04 17:19:32 +0700

    Front End XML done

--- app/src/main/java/com/arboy/kalkulator/MainActivity.kt
+++ app/src/main/java/com/arboy/kalkulator/MainActivity.kt
@@ -2,10 +2,27 @@ package com.arboy.kalkulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onAllclearClick(view: View) {}
+
+
+    fun onEqualClick(view: View) {}
+
+
+    fun onDigitClick(view: View) {}
+
+
+    fun onOperatorClick(view: View) {}
+
+
+    fun onBackClick(view: View) {}
+
+    fun onClearClick(view: View) {}
 }
